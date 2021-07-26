42da3a219e755e0d5975bb6dc7deeb0daa3d292e
iganchev <iganchev@infragistics.com>
2021-07-26 15:45:34 +0300

    Initial commit for project: IG Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { IgxLayoutModule, IgxNavbarModule, IgxNavigationDrawerModule, IgxRippleModule } from 'igniteui-angular';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+],
+imports: [
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HammerModule,
+    AppRoutingModule,
+    IgxLayoutModule,
+    IgxNavbarModule,
+    IgxNavigationDrawerModule,
+    IgxRippleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
